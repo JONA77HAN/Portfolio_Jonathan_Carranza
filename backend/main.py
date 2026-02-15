d1bc5db27e9b0db6db2aef06ad13f52540e40cba
Jonathan <jonathancarranza70@gmail.com>
2026-02-15 13:48:12 -0300

    dependencias agregadas a requirements

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,19 @@
-#logica de API
-
 from fastapi import FastAPI
 import requests
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="MeLi Portfolio API")
 
-# Permitir que tu Frontend (HTML/JS) se conecte al Backend
+# 1. Configuración de CORS Mejorada
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=[
+        "https://portfolio-jonathan-carranza-scpe.vercel.app",
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+        "*" 
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -20,15 +24,12 @@ def home():
 
 @app.get("/mercado-libre/tendencias/{category_id}")
 async def get_trends(category_id: str):
-    """
-    Obtiene tendencias reales de una categoría para mostrar en tu web.
-    Ejemplo de category_id: MLA1055 (Celulares)
-    """
+    """Obtiene tendencias reales de MeLi"""
     url = f"https://api.mercadolibre.com/trends/MLA/{category_id}"
     response = requests.get(url)
     
     if response.status_code == 200:
-        return response.json()[:5]  # Devolvemos solo las primeras 5 tendencias
+        return response.json()[:5]
     return {"error": "No se pudo conectar con MeLi"}
 
 @app.get("/mercado-libre/producto/{item_id}")
